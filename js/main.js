aa110f10f0963741d856d4d233a4edc528046533
bmsmostafiz <bms.mostafiz@gmail.com>
2023-09-19 19:35:08 +0600

    done

--- js/main.js
+++ js/main.js
@@ -1,59 +1,58 @@
-
-
-$(document).ready(function(){
-    $('.slider1').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-
-
-
-    $('.slider2').slick({
-        centerMode: true,
-        centerPadding: '20px',
-        slidesToShow: 2,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: '40px',
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
-
-
-
-      $('.slider3').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
-          
-
-
-      $('.slider4').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1
-      });
-
-
-
-
+$(document).ready(function () {
+  $('.slider1').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
   });
+
+
+
+  $('.slider2').slick({
+    centerMode: true,
+    centerPadding: '20px',
+    slidesToShow: 2,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+
+
+  $('.slider3').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+
+
+  $('.slider4').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+
+
+
+});
